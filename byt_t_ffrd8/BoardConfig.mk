c498a7c1e5ed8da44bc207069a517a97cc4fef09
Sridhar Uyyala <sridhar.uyyala@intel.com>
2014-02-04 18:11:48 -0800

    Enabling byt_t_ffrd8 in ABT
    Author: Sridhar Uyyala <sridhar.uyyala@intel.com>
    Author: Scott D Phillips <scott.d.phillips@intel.com>
    
    Adding necessary board config files to enable
    byt_t_ffrd8 support.
    
    For: APDEV-1335
    Change-Id: I93e9a4046f74f9e11fa29f3fec1aad433a69fb21
    Signed-off-by: Sridhar Uyyala <sridhar.uyyala@intel.com>
    Reviewed-on: https://android.intel.com/160425
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>
    Tested-by: Boie, Andrew P <andrew.p.boie@intel.com>

--- byt_t_ffrd8/BoardConfig.mk
+++ byt_t_ffrd8/BoardConfig.mk
@@ -1,6 +1,11 @@
 include device/intel/baytrail/BoardConfig.mk
 
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
+TARGET_USE_MOKMANAGER := true
+
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
+TARGET_IAGO_DEFAULT_INI := device/intel/baytrail/byt_t_ffrd8/iago-default.ini
+
+KERNEL_LOGLEVEL := 7
 
 TARGET_KERNEL_SOURCE := linux/kernel
 TARGET_KERNEL_ARCH := i386
@@ -10,10 +15,9 @@ TARGET_KERNEL_CONFIG := $(TARGET_KERNEL_ARCH)_byt_defconfig
 # config, if Iago not used you will need to add
 # androidboot.disk=80860F14:00
 BOARD_KERNEL_CMDLINE += \
-		androidboot.sdcard=mmcblk1 \
-		androidboot.disk=pci0000:00/0000:00:17.0
+		androidboot.sdcard=sda \
 
 # Display and backlight Setting for Kernel
-BOARD_KERNEL_CMDLINE += i915.mipi_panel_id=3 i915.i915_rotation=1 \
+BOARD_KERNEL_CMDLINE += vga=current i915.mipi_panel_id=3 i915.i915_rotation=1 \
                         i915.modeset=1 \
                         acpi_backlight=vendor
--- byt_t_ffrd8/byt_t_ffrd8.mk
+++ byt_t_ffrd8/byt_t_ffrd8.mk
@@ -13,8 +13,12 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-mixin, gms, false)
 $(call inherit-mixin, houdini, true)
-$(call inherit-mixin, boot-arch, sfi)
-$(call inherit-mixin, graphics, software)
+$(call inherit-mixin, boot-arch, efi)
+$(call inherit-mixin, graphics, ufo)
+$(call inherit-mixin, video, ufo)
 $(call inherit-mixin, display-density, high)
+$(call inherit-mixin, ethernet, static)
+$(call inherit-mixin, navigationbar, true)
 $(call inherit-mixin, touch, max_1664S_8)
+$(call inherit-mixin, device-type, tablet)
 
